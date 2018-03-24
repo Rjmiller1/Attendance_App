4c56c9d194b18f8714888a0f2e2879a1feead928
Robert Miller <robertmiller@roberts-macbook-pro-2.local>
2018-03-24 14:36:30 -0400

    Initial Commit

---
+++ Attendance_App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Attendance_App
+//
+//  Created by Robert Miller on 3/24/18.
+//  Copyright © 2018 Robert Miller. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
