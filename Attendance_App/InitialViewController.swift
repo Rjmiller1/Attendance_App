4ea6c34bad9b0a79296d60bd74ec8250f2c13f9d
Robert Miller <robertmiller@roberts-macbook-pro-2.local>
2018-03-24 14:51:33 -0400

    Initial push including initial view controller and login view controller

---
+++ Attendance_App/InitialViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  Attendance_App
+//
+//  Created by Robert Miller on 3/24/18.
+//  Copyright © 2018 Robert Miller. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class InitialViewController: UIViewController {
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        //- Todo: Check if user is authenticated. If so, segue to the HomeViewController, otherwise, segue to the MenuViewController
+        
+        self.performSegue(withIdentifier: "toMenuScreen", sender: self)
+    }
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        get {
+            return .lightContent
+        }
+    }
+}
