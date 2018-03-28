1a4a03caee5b50a132e40ba29be184b31a944084
Nicholas Furbee <nfurbee@Nicholass-MacBook-Pro.local>
2018-03-27 22:55:58 -0400

    New Controller

---
+++ Attendance_App/InstructorHomeViewController.swift
@@ -0,0 +1,9 @@
+//
+//  InstructorHomeViewController.swift
+//  Attendance_App
+//
+//  Created by Nicholas Furbee on 3/27/18.
+//  Copyright © 2018 Robert Miller. All rights reserved.
+//
+
+import Foundation
